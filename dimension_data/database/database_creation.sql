a1508a51b903c50891fabc35a4bc3f31f9312899
Mattews Michael <99986456+MichaelMattews@users.noreply.github.com>
2024-05-07 20:33:37 +0200

    Update database_creation.sql

--- dimension_data/database/database_creation.sql
+++ dimension_data/database/database_creation.sql
@@ -17,3 +17,53 @@ category_description VARCHAR(80) NOT NULL UNIQUE,
 );
 
 --------------------------------------
+
+
+ALTER TABLE "transaction"
+ADD CONSTRAINT unique_transaction_type_id UNIQUE (transaction_type_id);
+
+ALTER TABLE "category"
+ADD CONSTRAINT unique_category_id UNIQUE (category_id);
+
+
+--------------------------------------
+-- create a date_dimension table
+--------------------------------------
+
+CREATE TABLE IF NOT EXISTS date_dimension (
+transaction_date DATE PRIMARY KEY NOT NULL,
+    weekday_name VARCHAR(10) NOT NULL,
+    day_month INT NOT NULL,
+    month_name VARCHAR(10) NOT NULL,
+    quarter INT NOT NULL,
+    year INT NOT NULL
+);
+
+--------------------------------------
+-- create a transaction_facts table
+--------------------------------------
+
+CREATE TABLE IF NOT EXISTS transaction_facts (
+  transaction_id SERIAL PRIMARY KEY,
+  transaction_type_id INT NOT NULL,
+  category_id INT NULL,
+  transaction_date DATE NOT NULL,
+  transaction_description VARCHAR(100),
+  beneficiary_name VARCHAR(100),
+  transaction_amount DECIMAL NOT NULL,
+  CONSTRAINT fk_transaction
+    FOREIGN KEY (transaction_type_id)
+    REFERENCES transaction(transaction_type_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_category
+    FOREIGN KEY (category_id)
+    REFERENCES category (category_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_date_dimension
+    FOREIGN KEY (transaction_date)
+    REFERENCES date_dimension (transaction_date)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
